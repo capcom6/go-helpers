f3ec1efbaa105868fbb1813180033aaa80976b0e
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-02-10 17:50:57 +0700

    [slices] optimize `Random` for sigle-value slice

--- slices/random.go
+++ slices/random.go
@@ -10,6 +10,9 @@ func Random[T any](slice []T) (T, error) {
 	if len(slice) == 0 {
 		return *new(T), errors.New("slice is empty")
 	}
+	if len(slice) == 1 {
+		return slice[0], nil
+	}
 
 	return slice[rand.Intn(len(slice))], nil
 }
