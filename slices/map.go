09ae3278c95425604f42e93b531456c97f5f9473
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-07-01 06:35:56 +0700

    [slices] add `MapOrError` function

--- slices/map.go
+++ slices/map.go
@@ -12,3 +12,21 @@ func Map[T, U any](slice []T, f func(T) U) []U {
 	}
 	return result
 }
+
+// MapOrError applies the given function to each element of the input slice,
+// collecting results or returning the first error encountered.
+//
+// slice: The input slice of type T.
+// f: The function that maps elements of type T to elements of type U and may return an error.
+// Returns a slice with values of type U or an error if one occurred.
+func MapOrError[T, U any](slice []T, f func(T) (U, error)) ([]U, error) {
+	result := make([]U, len(slice))
+	for i, v := range slice {
+		u, err := f(v)
+		if err != nil {
+			return nil, err
+		}
+		result[i] = u
+	}
+	return result, nil
+}
